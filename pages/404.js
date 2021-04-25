b8b9fb81a7ef5a217a1a215d1424fe0f5eeacb49
Nadeen123 <nadeenshaheen.shahin@gmail.com>
2021-04-25 17:28:10 +0300

    error page

---
+++ pages/404.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Error = () => {
+    return (
+        <div>
+        <h1 class="p-3 mb-2 bg-danger text-white text-center">ERROR</h1>
+        <h1 class="text-danger text-center">Page Not Found !!</h1>  
+        <img src="/sad.gif"  />
+        </div>
+    )
+}
+
+export default Error
